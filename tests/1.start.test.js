caf18cf3db7965acb9ff38f946a72e5a2baec00f
Paul Mölders <paulmoelders@gmail.com>
2020-04-25 16:23:02 +0200

    explain tasks more

--- tests/1.start.test.js
+++ tests/1.start.test.js
@@ -19,24 +19,44 @@ describe("Start", () => {
 
   test("#1", () => {
     // SETUP
-    function sum(a, b) { return a + b; };
-    // YOUR SOLUTION
+    function sum(a, b) {
+      return a + b;
+    }
 
-    const answer = 0;
+    // AUFGABE: Ändere den Wert von answer so, dass der CHECK erfolgreich ist
 
-    // CHECK: DONT CHANGE THE NEXT LINE
+    const answer = 1;
+
+    // CHECK: diese Zeile macht folgendes: Prüfung ob das Ergebnis von sum(3, answer) 7 entspricht.
+    // Erklärung: expect() wertet etwas aus und .toBe()  vergleicht das Ergebnis mit einem Wert
+    // expect(1).toBe(1) wird also korrekt sein (PASS)
+    // expect(3).toBe(199) ergibt falsch (FAIL)
     expect(sum(3, answer)).toBe(7);
   });
 
   // dieser Test wird nur ausgeführt wenn du das x vor test entfernst: test("#2", ...
   xtest("#2", () => {
-    // YOUR SOLUTION
+    // Aufgabe: Ändere die Definition von der Funktion answer so, dass der CHECK erfolgreich ist.
 
-    const answer = (a) => a;
+    function answer(a) {
+      return a * 15;
+    }
 
-    // DONT CHANGE THE NEXT LINE
+    // CHECK:
     expect(answer(2)).toBe(6);
     expect(answer(3)).toBe(9);
     expect(answer(4)).toBe(12);
+  });
+
+  // dieser Test wird nur ausgeführt wenn du das x vor test entfernst: test("#3", ...
+  test("#3", () => {
+    // Aufgabe: Ändere die Definition von der Funktion answer so, dass der CHECK erfolgreich ist.
+
+    const answer = (a, b) => a + b;
+
+    // CHECK:
+    expect(answer(8, 1)).toBe(7);
+    expect(answer(4, 2)).toBe(2);
+    expect(answer(4, 0)).toBe(4);
   });
 });
