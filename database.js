e0c6c0012dba494c0b3ed2adb842a487b4c497cd
Paul Mölders <paulmoelders@gmail.com>
2020-04-29 20:32:10 +0200

    adds database.js

---
+++ database.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+// npm install --save mongoose
+
+const messageSchema = new mongoose.Schema({
+  text: String,
+  sender: String,
+  likes: Number,
+});
+
+const MessageModel = mongoose.model("Message", messageSchema);
+
+mongoose.connect("mongodb://localhost:27017/travelBlog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+module.exports.bootstrap = function bootstrap() {
+  return new Promise((resolve, reject) => {
+    mongoose.connection.once("open", () => {
+      // Database Connection established
+      resolve();
+    });
+  })
+}
+
+// alle Nachrichten aus der Datenbank:
+module.exports.getMessages = function getMessages() {
+  return MessageModel.find();
+}
